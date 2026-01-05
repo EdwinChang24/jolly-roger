1bb5d98f3a22785b1860b4caeb726a625aa81882
Edwin Chang <edwinchang2024@gmail.com>
2026-01-04 23:16:32 -0800

    fix a font

--- imports/client/components/styling/constants.tsx
+++ imports/client/components/styling/constants.tsx
@@ -1,6 +1,6 @@
 export const NavBarHeight = "50px";
 
 export const MonospaceFontFamily =
-  '"Platform Emoji", "Source Code Pro", "Consolas", "Monaco", monospace, "Noto Color Emoji", "Apple Color Emoji", "Segoe UI Emoji"';
+  '"Platform Emoji", "JetBrains Mono", "Consolas", "Monaco", monospace, "Noto Color Emoji", "Apple Color Emoji", "Segoe UI Emoji"';
 
 export const PuzzlePagePadding = "8px";
